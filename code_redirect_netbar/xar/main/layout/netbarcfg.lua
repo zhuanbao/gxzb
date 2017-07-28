0a0a2ed9bbaa0f47b995fa6224c1232a34d709ce
zhuanbao <ethminer@163.com>
2017-07-28 17:08:24 +0800

    1.测试openid与实际openid

--- code_redirect_netbar/xar/main/layout/netbarcfg.lua
+++ code_redirect_netbar/xar/main/layout/netbarcfg.lua
@@ -1,6 +1,7 @@
 function GetNetBarCfg()
 	local tabInfo = {}
 	tabInfo["strAgencyOpenID"] = "ojXBrw-eBXJLlm4hDIm5rkim4L2k"
+	--tabInfo["strAgencyOpenID"] = "ojXBrw1IQpt5MDocQbeRoBwdqMg4"
 	return tabInfo
 end
 
